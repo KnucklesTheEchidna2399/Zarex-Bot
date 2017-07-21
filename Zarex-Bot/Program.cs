7be66c1644913c218ade18d8551d345cbae85b05
sajidur rahaman <sajidur78@gmail.com>
2017-07-21 20:50:43 +0600

    Zarex

---
+++ Zarex-Bot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using System.Xml;
+using Discord;
+
+namespace Zarex_Bot
+{
+    class Program
+    {
+        public static string token;
+
+        static void Main(string[] args)
+        {
+            var Reader = XmlReader.Create("Appplication.xml");
+            while (Reader.Read()) {
+                switch (Reader.NodeType) {
+                    case XmlNodeType.Text:
+                        token = Reader.Value;
+                        break;
+                }
+            }
+            Console.WriteLine(token);
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync() {
+            var Client = new DiscordSocketClient();
+            Client.Log += Log;
+            await Client.LoginAsync(TokenType.Bot, token);
+            await Client.StartAsync();
+        }
+
+
+        private Task Log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
